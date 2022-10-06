655342c76bce2ba921d7175142f55a8dbc173289
fabio <fabio.matosteixeira@gmail.com>
2022-10-06 20:37:39 -0300

    Calculadora de Frete

---
+++ index.js
@@ -0,0 +1,9 @@
+function calculaFrete(endereço , produto) {
+
+     console.log(`Olá, boas vindas à nossa Loja. 
+     Já recebemos as informações e iremos mandar o 
+     produto ${produto} para ${endereço}.`)
+    
+}
+
+calculaFrete("São Paulo", "Camiseta")
