fc7f712c33378d47bbe05740525f691fb008db7a
fabio <fabio.matosteixeira@gmail.com>
2022-10-13 19:35:20 -0300

    Melhoramento da calculadora

---
+++ calculadora-v2.js
@@ -0,0 +1,26 @@
+const PREÇO_ESTADO = {
+    //Sudeste
+    SP: 'R$5,00',
+    RJ: 'R$6,50',
+    MG: 'R$7,20',
+    ES: 'R$8,00',
+    //Sul
+    PR: 'R$8,00',
+    SC: 'R$10,00',
+    RS: 'R$10,00',
+    //Norte
+    AM: 'R$23,50',
+    AC: 'R$23,50',
+    RO: 'R$22,00',
+    RR: 'R$22,00',
+    PA: 'R$20,00',
+    AP: 'R$20,00',
+    TO: 'R$20,00',
+    //Centro Oeste
+    GO: 'R$11,50',
+    MT: 'R$11,50',
+    MS: 'R$11,50',
+    //Nordeste R$15,00
+}
+
+const PREÇO_SAOPAULO = 'R$0,00'
